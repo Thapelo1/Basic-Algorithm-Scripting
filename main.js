8163a75a7021f6f9b73e29c9c1c863451daa48ee
Thapelo <Thapelo.Kekana1@fnb.co.za>
2022-07-12 23:28:09 +0200

    reverse-string

--- main.js
+++ main.js
@@ -1,9 +1,5 @@
-function convertToF(celsius) {
-    let fahrenheit = celsius * 9 / 5 + 32;
-    return fahrenheit;
-  }
-
-  console.log(convertToF(30)); // Should print 86
-
-  // The formula to convert from Celsius to Fahrenheit is the temperature in Celsius times 9/5, plus 32.
-
+    function reverseString(str) {
+        return str.split("").reverse().join("");
+    }
+    
+    console.log(reverseString("hello")); // Should print olleh
